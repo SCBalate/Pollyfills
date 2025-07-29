abbaa004ce7ba562c1ab3c63b2738d96271b0355
SCBalate <git@stackblitz.com>
2025-07-29 11:24:16 +0530

    Initial commit

---
+++ src/pollyfills.js
@@ -0,0 +1,117 @@
+export function setupCounter(element) {
+  //Map custom
+
+  Array.prototype.myMap = function (cb) {
+    let newArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      newArr.push(cb(this[i], i, this));
+    }
+    return newArr;
+  };
+
+  // Filter
+
+  Array.prototype.myFilter = function (cb) {
+    let filterArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (cb(this[i], i, this)) {
+        filterArr.push(this[i]);
+      }
+    }
+    return filterArr;
+  };
+
+  // Reduce
+
+  Array.prototype.myReduce = function (cb, initialValue) {
+    let acc;
+    let startIndex;
+
+    if (initialValue !== undefined) {
+      acc = initialValue;
+      startIndex = 0;
+    } else {
+      if (this.length === 0) {
+        throw new Error('reduce of empty array with no initial value');
+      }
+      acc = this[0];
+      startIndex = 1;
+    }
+    for (let i = startIndex; i < this.length; i++) {
+      acc = acc ? cb(acc, this[i], i, this) : this[i];
+    }
+    return acc;
+  };
+
+  // Call
+
+  Function.prototype.myCall = function (context = {}, ...args) {
+    if (typeof this !== 'function') {
+      throw new Error('this is not callable');
+    }
+    context.fn = this;
+    context.fn(...args);
+  };
+
+  Function.prototype.myApply = function (context = {}, args = []) {
+    if (typeof this !== 'function') {
+      throw new Error('this is not callable');
+    }
+    if (Array.isArray(args)) {
+      throw new Error('CreateList of array like call on non object');
+    }
+
+    context.fn = this;
+    context.fn(...args);
+  };
+
+  Function.prototype.mybind = function (context = {}, ...args) {
+    if (typeof this !== 'function') {
+      throw new Error('this is not callable');
+    }
+    fn = this;
+    return function (...newArgs) {
+      return context.apply(context, [...args, ...newArgs]);
+    };
+  };
+
+  Function.prototype.once = function (fn) {
+    let called = false;
+    let res;
+
+    return function (...args) {
+      if (!called) {
+        res = fn.apply(this, args);
+        called = true;
+      }
+      return res;
+    };
+  };
+
+  Function.prototype.memoize = function (fn) {
+    let res = {};
+
+    return function (...args) {
+      const cachedRes = JSON.stringify(args);
+      if (!res[cachedRes]) {
+        res[cachedRes] = fn.apply(this, args);
+      }
+      return res[cachedRes];
+    };
+  };
+
+  function myDebounce(fn, delay) {
+    let timer;
+    return function (...args) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+      }, delay);
+    };
+    return timer;
+  }
+}
